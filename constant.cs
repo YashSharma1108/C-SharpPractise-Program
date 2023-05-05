2ab36cfe1f6d6390fef58d615d6cfb59621cc03e
YashSharma1108 <132542971+YashSharma1108@users.noreply.github.com>
2023-05-05 09:20:49 +0530

    Add files via upload

---
+++ constant.cs
@@ -0,0 +1,10 @@
+// using System;
+// class circle{
+//     const float pi=3.14f;
+//     public static void Main(string[] args){
+//         float r=float.Parse(Console.ReadLine());
+//         circle c=new circle();
+//         float area=pi*r*r;
+//         Console.Write(area);
+//     }
+// }
